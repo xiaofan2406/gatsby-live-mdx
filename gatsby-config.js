ed0a1b9dcd63f473e6fe3d33da3b0c8cd19e23ec
Xiaofan Wu <xiaofan2406@gmail.com>
2020-03-17 14:59:14 +1100

    chore: mdx extentions

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,6 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-mdx',
       options: {
-        extensions: ['.mdx', '.md'],
         defaultLayouts: {
           default: require.resolve('./src/components/Layout.js'),
         },
